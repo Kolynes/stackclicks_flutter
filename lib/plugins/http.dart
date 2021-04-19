9c9b601cda488b5c0b2f08026ebbc53d354e1288
Kolynes Chinedu <kolyneschinedu@gmail.com>
2021-04-19 23:49:19 +0100

    update

--- lib/plugins/http.dart
+++ lib/plugins/http.dart
@@ -1,12 +1,12 @@
 import 'package:requests/requests.dart';
 import 'package:stackclicks_flutter/utils/Http.dart';
 
-// final protocol = "https";
-final protocol = "http";
-// final hostname =  "stackclicks.com.ng";
-final hostname =  "192.168.43.234";
-// final port = "";
-final port = ":8000";
+final protocol = "https";
+// final protocol = "http";
+final hostname =  "stackclicks.com.ng";
+// final hostname =  "192.168.43.234";
+final port = "";
+// final port = ":8000"
 
 final http = Http(
   Settings({
